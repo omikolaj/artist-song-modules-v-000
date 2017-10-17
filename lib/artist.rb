32e560d153b39c17f7dce6b4fe402a48a8ed4433
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-17 10:34:33 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,11 +9,7 @@ class Artist
 
   @@artists = []
 
-=begin
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-=end
+
   def initialize
     @@artists << self
     @songs = []
@@ -31,9 +27,5 @@ class Artist
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-=begin
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-=end
+
 end
