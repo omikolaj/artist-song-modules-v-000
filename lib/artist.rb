0f420828591403b5684e842dff19ac0d6d88c23b
Oskar Mikolajczyk <omikolaj1@gmail.com>
2017-10-17 10:05:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,14 +18,16 @@ class Artist
   def self.all
     @@artists
   end
-
+=begin
   def self.reset_all
     self.all.clear
   end
-
+=end
+=begin
   def self.count
     @@artists.count
   end
+=end
 
   def add_song(song)
     @songs << song
